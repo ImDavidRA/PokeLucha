754c39c5f0575ea928e5133ae8f54b068d9934a0
Esteban Martínez-Abarca Gabarda <esteban.martinez-abarca.gabarda@ces-vegamedia.es>
2024-02-27 11:24:42 +0100

    Añadida la evasion y los atributos al zombi saltarin

--- Juego_Zombi/src/juegoZombie/ZombieHijo3.java
+++ Juego_Zombi/src/juegoZombie/ZombieHijo3.java
@@ -1,11 +1,24 @@
 package juegoZombie;
 
 public class ZombieHijo3 extends Zombie {
-	
+	// Atributos
+	private int daño;
+	private int salud;
+	private int velocidad;
+	private String nombre;
+	// Constructor
 	ZombieHijo3() {
 		this.daño=75;
-		this.salud=300;
-		this.nombre="Zombi3";
+		this.salud=150;
+		this.nombre="Saltarin";
 		this.velocidad=4;
 	}
+	
+	// Metodos
+	public void evasion(int x) {
+		if ((int)Math.random()*10+1==1) {
+			salud=salud+x;
+		}
+	}
+	
 }
