b7eec884fcf8f1efc4a32af6ee5771e2b1b499c9
Krilin97 <miguelangel.espinosa.luna@ces-vegamedia.es>
2024-02-27 17:48:09 +0100

    Actualizado Para subir

--- Juego_Zombi/src/juegoZombie/zombieCorredor.java
+++ Juego_Zombi/src/juegoZombie/zombieCorredor.java
@@ -3,8 +3,8 @@ package juegoZombie;
 public class zombieCorredor extends Zombie {
 	
 	zombieCorredor() {
-		this.daño=100;
-		this.salud=200;
+		this.daño=170;
+		this.salud=180;
 		this.nombre="Zombi2";
 		this.velocidad=7;
 	}
